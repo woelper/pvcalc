f99c9e962648212390bc9e1f309f7cc4251de457
Johann Woelper <woelper@gmail.com>
2024-01-21 23:11:29 +0100

    improved translation

--- src/translation.rs
+++ src/translation.rs
@@ -11,17 +11,38 @@ macro_rules! tr {
 
     ($($arg:tt)*) => {{
 
-        // let mut dict : HashMap<String,HashMap<String,String>> = std::fs::File::open("translations.json").map(|f| serde_json::from_reader(f).unwrap_or_default()).unwrap_or_default();
 
+        if false
+        {
+            let mut dict : std::collections::HashMap<String, std::collections::HashMap<String,String>> = std::fs::File::open("translations.json").map(|f| serde_json::from_reader(f).unwrap_or_default()).unwrap_or_default();
+            let pattern_str = stringify!($($arg)*).to_string().replace("\"","");
+            let args =  pattern_str.split(',').nth(0).unwrap_or(&pattern_str);
+            println!("Pattern before expansion: {}", args);
+            let locale = sys_locale::get_locale().unwrap_or("de".to_string());
+            dict.entry(args.to_string()).or_default().insert(locale, args.to_string());
+            _ = serde_json::to_writer_pretty(std::fs::File::create("translations.json").unwrap(), &dict);
+        }
+        
+       
         let res = format!($($arg)*);
         // let res = format!($($arg)*);
-        // let locale = sys_locale::get_locale().unwrap_or("de".to_string());
-        // dict.entry(res.clone()).or_default().insert(locale, res.clone());
 
-        // _ = serde_json::to_writer_pretty(std::fs::File::create("translations.json").unwrap(), &dict);
 
         // dict.insert(locale, )
 
         res
     }}
 }
+
+
+
+
+// #[macro_export]
+// macro_rules! tr {
+//     ($($arg:tt)*) => {{
+//         let input_str = stringify!($($arg)*);
+//         println!("Input before expansion: {}", input_str);
+//         let res = format!($($arg)*);
+//         res
+//     }}
+// }
